058fdb28c90bb0873907c771bbfa56f286c454be
zhanji-3644 <18826077372@163.com>
2026-07-09 16:02:29 +0800

    修正模型请求地址

--- src/views/Fullscreen3D.vue
+++ src/views/Fullscreen3D.vue
@@ -120,7 +120,7 @@ onMounted(() => {
   updateSunLight()
 
   const loader = new GLTFLoader()
-  loader.load('/models/ac.glb',
+  loader.load(import.meta.env.BASE_URL + 'models/ac.glb',
     (gltf) => {
       const model = gltf.scene
       const box = new THREE.Box3().setFromObject(model)
